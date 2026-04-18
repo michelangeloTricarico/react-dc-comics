2ce0f6172cf64367e526a54d32f30963c60d0ba6
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-04-18 17:32:45 +0200

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header.jsx'
+import Main from './components/main.jsx'
+import Footer from './components/footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
