8ddfe0499ef2564ba9a8a31012e449698202a9a7
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-04-19 09:54:43 +0200

    V0.0.5

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import DClogo from '../assets/dc-logo.png'
+import nav from "../data/nav.js"
 export default function Header(){
     return(
         <div className="row pt-3">
@@ -6,16 +7,9 @@ export default function Header(){
                 <img className="logo d-flex justify-content-start" src={DClogo} alt="" />
             </div>
             <div className="header_link col d-flex gap-3">
-                <a href="#" id="1">CHARACTERS</a>
-                <a href="#" id="2">COMICS</a>
-                <a href="#" id="3">MOVIES</a>
-                <a href="#" id="4">TV</a>
-                <a href="#" id="5">GAMES</a>
-                <a href="#" id="6>COLLECTIBLES</a>
-                <a href="#" id="7">VIDEOS</a>
-                <a href="#" className="fan_a">FANS</a>
-                <a href="#" className="new_a">NEWS</a>
-                <a href="#" className="shop_a">SHOP</a>
+                {nav.map((tab) => (
+                    <a href={tab["link"]} className={tab["class"]}>{tab["content"]}</a>
+                ))}
             </div>
         </div>
     );
--- src/data/nav.js
+++ src/data/nav.js
@@ -2,7 +2,61 @@ const nav=[
     {
         id: 1,
         content:"CHARACTERS",
-        class:""
-
+        class: chara_a,
+        link:"#"
+    },
+    {
+        id: 2,
+        content:"COMICS",
+        class: comics_a,
+        link:"#"
+    },
+    {
+        id: 3,
+        content:"MOVIES",
+        class: "movie_a",
+        link:"#"
+    },
+    {
+        id: 4,
+        content:"TV",
+        class: "tv_a",
+        link:"#"
+    },
+    {
+        id: 6,
+        content:"GAMES",
+        class: "games_a",
+        link:"#"
+    },
+    {
+        id: 7,
+        content:"COLLECTIBLES",
+        class: "collect_a",
+        link:"#"
+    },
+    {
+        id: 8,
+        content:"VIDEOS",
+        class: "video_a",
+        link:"#"
+    },
+    {
+        id: 9,
+        content:"FANS",
+        class: "fan_a",
+        link:"#"
+    },
+    {
+        id: 10,
+        content:"NEWS",
+        class: "new_a",
+        link:"#"
+    },
+    {
+        id: 11,
+        content:"SHOP",
+        class: "shop_a",
+        link:"#"
     }
 ]
